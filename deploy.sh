28f9ab7a0091dea991f98504e2eedd0a9b6e8899
Jany Nemcik <janynemcik5@gmail.com>
2019-02-20 16:03:23 +0100

    deploy process modified

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 echo "transfer started..."
 rsync -r --delete-after $TRAVIS_BUILD_DIR/dist deploy@46.101.209.6:dist
 echo "content transfered"
-ls -la
-whoami
 
+ssh -t deploy@iot.itprof.sk 'cd dist/dist; npm i; pm2 restart ./main.js; bash -l'
