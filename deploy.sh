318e986dd7f6c8f437e508c5c82254e6948f1377
Jany Nemcik <janynemcik5@gmail.com>
2019-02-20 15:14:51 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bash/bin
 
 echo "transfer started..."
-rsync -r --no-implied-dirs --delete-after $TRAVIS_BUILD_DIR/dist deploy@46.101.209.6:dist
+rsync -r --delete-after $TRAVIS_BUILD_DIR/dist deploy@46.101.209.6:dist
 echo "content transfered"
 ls -la
 whoami
