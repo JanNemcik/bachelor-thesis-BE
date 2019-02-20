1e5dcf7c2ca31f3aa78179623f2e4f88d5c5d091
Jany <janynemcik5@gmail.com>
2019-02-20 14:03:37 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
 #!/bash/bin
 
-ssh -oStrictHostKeyChecking=no deploy@46.101.209.6
-ls
-whoami
+ssh -oStrictHostKeyChecking=no deploy@46.101.209.6 | ls | whoami
 //rsync -r --delete-after /dist deploy@46.101.209.6:/var/www/iot-be 
