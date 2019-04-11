23cbfef13081ba19d2a60d3c2ed58cffe4aecafa
Jany Nemcik <janynemcik5@gmail.com>
2019-04-11 13:19:44 +0200

    edited startup script

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ echo "transfer started..."
 rsync -r --delete-after $TRAVIS_BUILD_DIR/dist deploy@104.248.18.71:dist
 echo "content transfered"
 
-ssh -t deploy@104.248.18.71 'cd dist/dist; npm i; pm2 restart ./main.js; exit'
+ssh -t deploy@104.248.18.71 'cd dist/dist; npm i; pm2 restart be-app || pm2 start ./main.js --name="be-app"; exit'
