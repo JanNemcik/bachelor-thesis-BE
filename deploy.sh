5754e967b3ee4980aa2973c3809745eedccd049c
Jany Nemcik <janynemcik5@gmail.com>
2019-04-11 14:17:52 +0200

    deploy script edited; renamed folder of app on server

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bash/bin
 
 echo "transfer started..."
-rsync -r --delete-after $TRAVIS_BUILD_DIR/dist deploy@104.248.18.71:dist
+rsync -r --delete-after $TRAVIS_BUILD_DIR/dist deploy@104.248.18.71:be-dist
 echo "content transfered"
 
-ssh -t deploy@104.248.18.71 'cd dist/dist; npm i; pm2 restart be-app || pm2 start ./main.js --name="be-app"; rm -rf dist; exit'
+ssh -t deploy@104.248.18.71 'cd be-dist/dist; npm i; pm2 restart be-app || pm2 start ./main.js --name="be-app"; exit'
