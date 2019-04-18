414dadb691979ce34e36c4eadcb9e733d302fc88
Jany Nemcik <janynemcik5@gmail.com>
2019-04-18 23:24:58 +0200

    fixed possible failure by deploying

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@ echo "transfer started..."
 rsync -r --delete-after $TRAVIS_BUILD_DIR/dist deploy@104.248.18.71:be-dist
 echo "content transfered"
 
-ssh -t deploy@104.248.18.71 'cd be-dist/dist; npm i; pm2 stop be-app && pm2 delete be-app; pm2 start ./main.js --name="be-app"; exit'
+ssh -t deploy@104.248.18.71 'cd be-dist/dist; npm i; pm2 stop be-app && pm2 delete be-app || echo "Application was not deployed yet"; pm2 start ./main.js --name="be-app"; exit'
