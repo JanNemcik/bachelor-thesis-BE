235178995b17874047ac76f5685020eb2ed75014
Jan Nemcik <janynemcik5@gmail.com>
2019-06-04 12:49:53 +0200

    mongo authentication added

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -1,37 +1,53 @@
-import { MongooseModuleAsyncOptions } from '@nestjs/mongoose';
+import {
+	MongooseModuleAsyncOptions,
+	MongooseModuleOptions
+} from '@nestjs/mongoose';
 import { IClientOptions } from 'mqtt';
 
+const MONGO_CONNECTIONS_OPTIONS: MongooseModuleOptions = {
+	useNewUrlParser: true,
+	user: 'bachelor',
+	pass: 'bachelor-iot',
+	dbName: 'bachelor-iot',
+	useCreateIndex: true
+};
+
 export const MQTT_CLIENT_OPTIONS: IClientOptions = {
-  port: 1883,
-  clientId:
-    'mqttjs_' +
-    Math.random()
-      .toString(16)
-      .substr(2, 8),
-  username: 'bachelor',
-  password: 'iotbachelor',
-  protocol: 'mqtt',
-  protocolId: 'MQTT',
-  host: '104.248.18.71',
-  connectTimeout: 2147483647
+	port: 1883,
+	clientId:
+		'mqttjs_' +
+		Math.random()
+			.toString(16)
+			.substr(2, 8),
+	username: 'bachelor',
+	password: 'iotbachelor',
+	protocol: 'mqtt',
+	protocolId: 'MQTT',
+	host: '104.248.18.71',
+	connectTimeout: 2147483647
 };
 
 export const DATABASE_OPTIONS_LOCAL: MongooseModuleAsyncOptions = {
-  useFactory: () => ({
-    uri: 'mongodb://localhost:27017/bachelor-iot'
-  })
+	useFactory: () => ({
+		uri: 'mongodb://localhost:27017/bachelor-iot'
+	})
 };
 
 export const DATABASE_OPTIONS_PROD: MongooseModuleAsyncOptions = {
-  useFactory: () => ({
-    uri: 'mongodb://iot.itprof.sk:27017/bachelor-iot'
-  })
+	useFactory: () => ({
+		uri: 'mongodb://iot.itprof.sk:27017/bachelor-iot'
+	})
 };
 
 export const DATABASE_OPTIONS_DEV: MongooseModuleAsyncOptions = {
-  useFactory: () => ({
-    uri: 'mongodb://104.248.18.71:27017/bachelor-iot'
-  })
+	useFactory: () => ({
+		uri: 'mongodb://104.248.18.71:27017/bachelor-iot',
+		useNewUrlParser: true,
+		user: 'bachelor',
+		pass: 'bachelor-iot',
+		dbName: 'bachelor-iot',
+		useCreateIndex: true
+	})
 };
 
 export const MQTT_MESSAGE_PSK = 'afoj';
